a3a1e4d6c28af714bb89691ade6fd7a5745bba58
zl <lsj515325@163.com>
2021-01-21 00:52:27 +0800

    Initial commit

---
+++ src/main/java/com/microservice/nacos/StudyGradleNacosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGradleNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGradleNacosApplication.class, args);
+    }
+
+}
