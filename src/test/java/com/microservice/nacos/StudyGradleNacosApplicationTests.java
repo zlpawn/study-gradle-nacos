09db36c2d53101632ca7690cfeece7db487067a9
zhanglin <lsj515325@163.com>
2021-01-21 20:14:12 +0800

    切换nacos地址

--- src/test/java/com/microservice/nacos/StudyGradleNacosApplicationTests.java
+++ src/test/java/com/microservice/nacos/StudyGradleNacosApplicationTests.java
@@ -1,13 +1,18 @@
 package com.microservice.nacos;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudyGradleNacosApplicationTests {
 
+    @Value("${pawn}")
+    private String pawn;
+
     @Test
     void contextLoads() {
+        System.err.println(pawn);
     }
 
 }
